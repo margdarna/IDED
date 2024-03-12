97633cafbce9d6152bfc5f55a507fcb1add12841
Margarita Darna <97949890+margdarna@users.noreply.github.com>
2024-03-12 14:43:38 +0100

    Update summary_stat.m

--- summary_stat.m
+++ summary_stat.m
@@ -7,29 +7,29 @@
 % Created by: Margarita Darna
 % margarita.darna@lin-magdeburg.de
 
-%%
-%----------------------------------------------------------------------
+%% --------------------------------------------------------------------
 %                  Prepare workspace and directories
 %----------------------------------------------------------------------
 % Cleaning workspace and command window
 clear;clc;
+
 % Setting up needed directories
+% adjust accordingly
 dirs = {};
-% change project_dir accordingly
-dirs.proj_dir = 'C:/your_project_directory/';  
+%dirs.proj_dir = 'C:\Users\mdarna\Documents\PhD\A05-SFB1436\';    
+dirs.proj_dir       = '//linstore01/home/mdarna/PhD/A05-SFB1436/';    
 dirs.dt_dir         = strcat (dirs.proj_dir, 'Data/');
 dirs.exp_dir        = strcat (dirs.proj_dir, 'IDED_v1_Analysis/');
 dirs.raw_dt_dir     = strcat(dirs.dt_dir, 'Raw_data/');
 dirs.derived_dt_dir = strcat(dirs.exp_dir, 'Data/Derived_data/');
-dirs.analysis_dir   = strcat(dirs.exp_dir, 'Functions/');
+dirs.analysis_dir   = strcat(dirs.exp_dir, 'Analysis/');
 dirs.output_dir     = strcat(dirs.exp_dir, 'Output/');
 dirs.prepr_dir      = strcat(dirs.output_dir, '1_Preprocessing/');
 
 % adding analysis path and subfolders
 addpath(genpath(dirs.exp_dir));
 
-%%
-%----------------------------------------------------------------------
+%% --------------------------------------------------------------------
 %            Load excel table with Subject Information
 %----------------------------------------------------------------------
 subj_info = readtable(strcat(dirs.exp_dir, 'Protocol.xlsx'));
@@ -40,8 +40,14 @@ is_young  = categorical(age_group) == 'young';
 is_old  = categorical(age_group) == 'old';
 subj_young = subs(is_young);
 subj_old = subs(~is_young);
-age= subj_info.age;
-gender = subj_info.Gender;
+
+dem_info =  readtable(strcat(dirs.raw_dt_dir, 'Demographics.xlsx'));
+% make sure that age and gender is assigned to the correct subject
+for i = 1:numel(subs)
+    subj = subs{i};
+    age(i) = dem_info.Age(strcmp(dem_info.Pseudonym,subj), :);
+    gender(i) = dem_info.Gender(strcmp(dem_info.Pseudonym,subj), :);
+end
 
 % display some summary information
 fprintf('Young:\nAge:   %.1f +- %.1f\n', mean(age(is_young)), std(age(is_young)))
@@ -56,8 +62,7 @@ fprintf('PSS10: %.1f +- %.1f\n', mean(subj_info.PSS10(is_old), 'omitnan'), std(s
 fprintf('MWTB:  %.1f +- %.1f\n', mean(subj_info.MWTB(is_old), 'omitnan'), std(subj_info.MWTB(is_old), 'omitnan'))
 fprintf('MMSE:  %.1f +- %.1f\n\n', mean(subj_info.MMSE(is_old), 'omitnan'), std(subj_info.MMSE(is_old), 'omitnan'))
 
-%% 
-%----------------------------------------------------------------------
+%% --------------------------------------------------------------------
 %      Create individual performance struct and load directories
 %----------------------------------------------------------------------
 task = "IDED";
@@ -67,7 +72,6 @@ perf_all.subs = subs;
 
 for n = 1: numel(task)
     fprintf('\nLoading files for %s:\n-----------------------------------\n', task(n));
-
     if task(n) == "IDED"
         perf_all.IDED.perf_dirs = get_perf_dirs(dirs.raw_dt_dir, dirs.derived_dt_dir, subs, task(n));
     else
@@ -75,26 +79,26 @@ for n = 1: numel(task)
     end
 end
 
-%% 
-%------------------------------------------------------------------------
+%% ----------------------------------------------------------------------
 %                   Retrieve summary statistics
 %------------------------------------------------------------------------
 perf_all = get_perf_all(dirs, task, perf_all);
 
 % print some overall results
-fprintf('IDED Young:\nReaction Time: %.0f +- %.0f ms\n', mean(perf_all.IDED.RT.median_all(is_young)) * 1000, std(perf_all.IDED.RT.median_all(is_young)) * 1000)
-fprintf('Error rate:    %.2f +- %.2f %% \n\n', mean(perf_all.IDED.error.error_all(is_young)) * 100, std(perf_all.IDED.error.error_all(is_young)) * 100)
-fprintf('IDED Old:\nReaction Time: %.0f +- %.0f ms\n', mean(perf_all.IDED.RT.median_all(is_old)) * 1000, std(perf_all.IDED.RT.median_all(is_old)) * 1000)
-fprintf('Error rate:    %.2f +- %.2f %% \n\n', mean(perf_all.IDED.error.error_all(is_old)) * 100, std(perf_all.IDED.error.error_all(is_old)) * 100)
+fprintf('IDED Young:\nReaction Time:    %.0f +- %.0f ms\n', mean(perf_all.IDED.RT.median_all(is_young)) * 1000, std(perf_all.IDED.RT.median_all(is_young)) * 1000)
+fprintf('RT St. Deviation: %.0f +- %.0f ms\n', mean(perf_all.IDED.RT.std_all(is_young)) * 1000, std(perf_all.IDED.RT.std_all(is_young)) * 1000)
+fprintf('Error rate:       %.2f +- %.2f %% \n\n', mean(perf_all.IDED.error.error_all(is_young)) * 100, std(perf_all.IDED.error.error_all(is_young)) * 100)
+
+fprintf('IDED Old:\nReaction Time:    %.0f +- %.0f ms\n', mean(perf_all.IDED.RT.median_all(is_old)) * 1000, std(perf_all.IDED.RT.median_all(is_old)) * 1000)
+fprintf('RT St. Deviation: %.0f +- %.0f ms\n', mean(perf_all.IDED.RT.std_all(is_old)) * 1000, std(perf_all.IDED.RT.std_all(is_old)) * 1000)
+fprintf('Error rate:       %.2f +- %.2f %% \n\n', mean(perf_all.IDED.error.error_all(is_old)) * 100, std(perf_all.IDED.error.error_all(is_old)) * 100)
 
 fprintf('IDED All:\nReaction Time: %.0f +- %.0f ms\n', mean(perf_all.IDED.RT.median_all) * 1000, std(perf_all.IDED.RT.median_all) * 1000)
-fprintf('Skewness: %.2f +- %.2f \n', mean(perf_all.IDED.RT.skewness_all), std(perf_all.IDED.RT.skewness_all))
+fprintf('Skewness:      %.2f +- %.2f \n', mean(perf_all.IDED.RT.skewness_all), std(perf_all.IDED.RT.skewness_all))
 fprintf('Error rate:    %.2f +- %.2f %% \n', mean(perf_all.IDED.error.error_all) * 100, std(perf_all.IDED.error.error_all) * 100)
-fprintf('Error rate:    %.2f +- %.2f %% \n\n', mean(perf_all.IDED.error.error_all) * 100, std(perf_all.IDED.error.error_all) * 100)
 
 
-%%
-%------------------------------------------------------------------------
+%% ----------------------------------------------------------------------
 %                       Create table
 %------------------------------------------------------------------------
 % Reaction time
@@ -121,6 +125,33 @@ T.within = [repmat('repeat', sum(is_young), 1); repmat('repeat', sum(~is_young),
 T.gender =  [gender(is_young)' - 1; gender(is_old)' - 1; gender(is_young)' - 1; gender(is_old)' - 1; ...
     gender(is_young)' - 1; gender(is_old)' - 1;];
 writetable(T, strcat(dirs.output_dir, '4_Stats\stat_RT_repeat_ID_ED.csv'));
+
+
+% Standard Deviation of Reaction time
+% use second repeat as control condition
+% separate by condition
+dv = [perf_all.IDED.RT.std_repeat2(is_young,:); perf_all.IDED.RT.std_repeat2(is_old,:); ...
+    perf_all.IDED.RT.std_ID(is_young,:); perf_all.IDED.RT.std_ID(is_old,:); ...
+    perf_all.IDED.RT.std_ED(is_young,:); perf_all.IDED.RT.std_ED(is_old,:)];
+subj_num = [1:numel(subs) 1:numel(subs) 1:numel(subs)]';
+
+X = [dv subj_num];
+
+% Create table to be saved
+T = array2table(X);
+% Default heading for the columns will be A1, A2 and so on. 
+% You can assign the specific headings to your table in the following manner
+T.Properties.VariableNames = {'dv','subj_num'};
+T.between = [repmat('young', sum(is_young), 1); repmat(' old ', sum(~is_young), 1) ;...
+    repmat('young', sum(is_young), 1); repmat(' old ', sum(~is_young), 1);
+    repmat('young', sum(is_young), 1); repmat(' old ', sum(~is_young), 1)];
+T.within = [repmat('repeat', sum(is_young), 1); repmat('repeat', sum(~is_young), 1); ...
+    repmat('  ID  ', sum(is_young), 1); repmat('  ID  ', sum(~is_young), 1);
+    repmat('  ED  ', sum(is_young), 1); repmat('  ED  ', sum(~is_young), 1)];
+T.gender =  [gender(is_young)' - 1; gender(is_old)' - 1; gender(is_young)' - 1; gender(is_old)' - 1; ...
+    gender(is_young)' - 1; gender(is_old)' - 1;];
+writetable(T, strcat(dirs.output_dir, '4_Stats\stat_RTstd_repeat_ID_ED.csv'));
+
 
 
 % Error rate
