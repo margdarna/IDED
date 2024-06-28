b0ef379df103879032fa7c2b4af28fa7fb4fd143
Margarita Darna <97949890+margdarna@users.noreply.github.com>
2024-06-28 12:48:57 +0200

    Update IDED_ERP_Analysis_Latency.m

--- Scripts/IDED_ERP_Analysis_Latency.m
+++ Scripts/IDED_ERP_Analysis_Latency.m
@@ -15,10 +15,9 @@
 clear;clc;
 % Setting up needed directories
 dirs = {};
-%dirs.proj_dir = 'C:\Users\mdarna\Documents\PhD\A05-SFB1436\';    
-dirs.proj_dir       = '//linstore01/home/mdarna/PhD/A05-SFB1436/';    
+dirs.proj_dir       = 'your_directory';
 dirs.dt_dir         = strcat (dirs.proj_dir, 'Data/');
-dirs.exp_dir        = strcat (dirs.proj_dir, 'IDED_v1_Analysis/');
+dirs.exp_dir        = strcat (dirs.proj_dir, 'IDED_Analysis/');
 dirs.raw_dt_dir     = strcat(dirs.dt_dir, 'Raw_data/');
 dirs.derived_dt_dir = strcat(dirs.dt_dir, 'Derived_data/IDED/');
 dirs.analysis_dir   = strcat(dirs.exp_dir, 'Analysis/');
